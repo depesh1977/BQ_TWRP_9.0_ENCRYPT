d34c44c9386df39c0c82a30fe2bc24d12ad92f09
depesh1977 <46925183+depesh1977@users.noreply.github.com>
2021-03-29 13:35:33 +0300

    Update BoardConfig.mk

--- k210/BoardConfig.mk
+++ k210/BoardConfig.mk
@@ -34,7 +34,7 @@ TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_SMP := true
-TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT := cortex-a53
 
 # Bootloader
 TARGET_NO_BOOTLOADER := true 
