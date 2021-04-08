5c43a8c5e3a77ff726bf4cb076ad4c6f1aca3624
depesh1977 <46925183+depesh1977@users.noreply.github.com>
2021-04-09 00:54:56 +0300

    Add files via upload

---
+++ k210/README.md
@@ -0,0 +1,1 @@
+# TWRP Device Tree for BQ k210 with the SpreadTrum sp7731e chipset
---
+++ k210/omni_k210.mk
@@ -0,0 +1,36 @@
+#
+# Copyright 2017 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Release name
+PRODUCT_RELEASE_NAME := k210
+
+$(call inherit-product, build/target/product/embedded.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := k210
+PRODUCT_NAME := omni_k210
+PRODUCT_BRAND := BQ
+PRODUCT_MODEL := K210
+PRODUCT_MANUFACTURER := BQ
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=K210 \
+    BUILD_PRODUCT=K210 \
+    TARGET_DEVICE=K210
