68ac68d83c97354866fd9d3c97e657299f3f3857
depesh1977 <46925183+depesh1977@users.noreply.github.com>
2024-03-29 20:43:42 +0300

    Update omni_BQ5519G.mk

--- omni_BQ5519G.mk
+++ omni_BQ5519G.mk
@@ -19,6 +19,7 @@ PRODUCT_RELEASE_NAME := BQ5519G
 
 # Inherit from those products. Most specific first.
 $(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 #$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 #$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
